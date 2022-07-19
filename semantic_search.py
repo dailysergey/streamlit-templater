b5be643b3b21653482033dc1f9fb2f90b0bc1828
Sergey Guskov <38214057+dailysergey@users.noreply.github.com>
2022-07-19 13:44:40 +0300

    imp

--- semantic_search.py
+++ semantic_search.py
@@ -65,8 +65,7 @@ def timeit(func):
 
 # Page layout
 # Page expands to full width
-st.set_page_config(page_title="ГИСП подбор шаблонов",
-                   page_icon="🗂", layout="wide")
+st.set_page_config(page_title="ГИСП подбор шаблонов", page_icon="🗂", layout="wide")
 st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">', unsafe_allow_html=True)
 
 
